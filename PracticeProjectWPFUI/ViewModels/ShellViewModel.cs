282b16ef45437f29627ac8fc103e7acd1ea0e5be
MI\ajoshi <arpita.joshi@milliman.com>
2023-05-26 09:53:11 -0500

    tables added, login screen functionality added

---
+++ PracticeProjectWPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace PracticeProjectWPFUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
+    }
+}
