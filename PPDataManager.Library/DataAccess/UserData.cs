e5079c422a7e5333688d6226f65543a6931c4c77
MI\ajoshi <arpita.joshi@milliman.com>
2023-07-19 13:10:18 -0500

    Login with error handling and getting user data

---
+++ PPDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using PPDataManager.Library.Internal.DataAccess;
+using PPDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "PPData");
+
+            return output;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using PPDataManager.Library.DataAccess;
+using PPDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            return data.GetUserById(userId);
+        }
+    }
+}
