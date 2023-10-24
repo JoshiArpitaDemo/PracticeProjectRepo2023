ca942d2e1689c2ebeb1728837d84de03f8ab9d2d
Arpita Joshi <joshi.g.arpita@gmail.com>
2023-10-24 15:50:09 -0500

    Fix logoff Bug
    Fixes bug where the header was not being cleared on log off

--- PracticeProjectWPFUI/ViewModels/ShellViewModel.cs
+++ PracticeProjectWPFUI/ViewModels/ShellViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using PracticeProjectDesktopUI.Library.Api;
 using PracticeProjectDesktopUI.Library.Models;
 using PracticeProjectWPFUI.EventModels;
 
@@ -14,12 +15,14 @@ namespace PracticeProjectWPFUI.ViewModels
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
         private ILoggedInUserModel _user;
+        private IAPIHelper _apiHelper;
 
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, ILoggedInUserModel user)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, ILoggedInUserModel user, IAPIHelper apiHelper)
         {
             _events = events;
             _salesVM = salesVM;
-            _user = user;   
+            _user = user;
+            _apiHelper = apiHelper;
 
             _events.Subscribe(this);
             ActivateItem(IoC.Get<LoginViewModel>());
@@ -45,7 +48,8 @@ namespace PracticeProjectWPFUI.ViewModels
 
         public void LogOut()
         {
-            _user.LogOffUser();
+            _user.ResetUserModel();
+            _apiHelper.LogOffUser();
             ActivateItem(IoC.Get<LoginViewModel>());
             NotifyOfPropertyChange(() => IsLoggedIn);
         }
